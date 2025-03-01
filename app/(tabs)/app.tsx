56f4e50c5e7146d7e42edcaff5b2db7d23af388b
Jere <jerekoivisto7@gmail.com>
2025-03-01 17:38:11 +0200

    Some styling experimenting and textinput testing around.

--- app/(tabs)/app.tsx
+++ app/(tabs)/app.tsx
@@ -1,18 +1,22 @@
-import { Text, View, StyleSheet, ScrollView } from "react-native";
-import { TextInput } from "react-native";
+import { Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function NotesScreen() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View>
-        <Text style={styles.text}>Notes screen</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Type here"
-          keyboardType="numeric"
-        />
-      </View>
-    </ScrollView>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View>
+            <Text style={styles.text}>Notes screen</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Type here"
+              keyboardType="default"
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -29,7 +33,7 @@ const styles = StyleSheet.create({
     fontSize: 100,
   },
   textInput: {
-    height: '40%',
+    height: "40%",
     color: "#fff", // Why color does not work?
     borderColor: "gray",
     borderWidth: 1,
