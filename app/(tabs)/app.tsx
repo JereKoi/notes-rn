cb4d0382c5e6131faaa1d1e6d2bcc9986a993808
Jere <jerekoivisto7@gmail.com>
2025-03-04 17:45:58 +0200

    Did some saving for text. Trying to figure out how the tabs/stack works.

--- app/(tabs)/app.tsx
+++ app/(tabs)/app.tsx
@@ -1,25 +1,30 @@
-import React from "react"
+import React from "react";
 import { Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function NotesScreen() {
-const [value, onChangeText] = React.useState()
+  const [text, setText] = React.useState("");
 
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
-          <View>
-            <Text style={styles.text}>Notes screen</Text>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Type here..."
-              keyboardType="default"
-              multiline
-              textAlignVertical="top"
-              placeholderTextColor={"white"}
-              submitBehavior="newline"
-            />
-          </View>
+        <View>
+          <Text style={styles.text}>Notes screen</Text>
+          <TextInput
+            value={text}
+            style={styles.textInput}
+            placeholder="Type here..."
+            keyboardType="default"
+            multiline
+            textAlignVertical="top"
+            placeholderTextColor={"white"}
+            submitBehavior="newline"
+            onChangeText={(text) => {
+              setText(text);
+              console.log("Saved text:", text)
+            }}
+          />
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
