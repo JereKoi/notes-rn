e1a8ff4d47567e27444de2e9d5715114fb65798b
Jere <jerekoivisto7@gmail.com>
2025-03-06 17:13:01 +0200

    Initial work on list component. I will develop a card grid that displays previous entries.

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,30 +1,31 @@
-import React from "react"
-import { Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  SectionList,
+} from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function list() {
   const [text, setText] = React.useState("");
 
+  const DATA = [
+    {
+      title: "06.03.2025",
+      data: ["hello world"],
+    },
+  ];
+
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
-        <View>
-          <Text style={styles.text}>Notes screen</Text>
-          <TextInput
-            value={text}
-            style={styles.textInput}
-            placeholder="Type here..."
-            keyboardType="default"
-            multiline
-            textAlignVertical="top"
-            placeholderTextColor={"white"}
-            submitBehavior="newline"
-            onChangeText={(text) => {
-              setText(text);
-              console.log("Saved text:", text)
-            }}
-          />
-        </View>
+        <SectionList
+          sections={DATA}
+          keyExtractor={(item, index) => item + index}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
