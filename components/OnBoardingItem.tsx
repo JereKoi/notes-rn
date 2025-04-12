7e274d02e000fb0b09f97a50e8dd7037f08b1934
Jere <jerekoivisto7@gmail.com>
2025-04-12 16:50:00 +0300

    Programming onboarding.

--- components/OnBoardingItem.tsx
+++ components/OnBoardingItem.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Text, View, StyleSheet, ScrollView, TextInput, Image, useWindowDimensions } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function OnBoardingItem() {
+
+// TODO: What to do to 'OnBoardingItem', which lacks return-type annotation, implicitly has an 'any' return type.ts(7010)
+// TODO: and Binding element 'item' implicitly has an 'any' type.ts(7031)
+
+export default function OnBoardingItem = ({{item} }) => {
   const [text, setText] = React.useState("");
+  const { width } = useWindowDimensions();
 
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
-        <View>
-          <Text style={styles.text}>OnBoardingItem</Text>
+        <View style={[styles.container, {width }]}>
+        <Image source={item.image} style={[styles.image, { width, resizeMode: "contain" }]}/>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -34,4 +39,8 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     borderWidth: 1,
   },
+
+  image: {
+    
+  }
 });
